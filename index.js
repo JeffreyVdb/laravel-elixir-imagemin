92b4f431f1579f14f570ae0068146a5a939764ed
Jeffrey Vandenborne <vandenbornejeffrey@gmail.com>
2015-03-04 14:50:29 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var PLUGIN_NAME  = 'imagemin',
+    _            = require('lodash'),
+    gulp         = require('gulp'),
+    imagemin     = require('gulp-imagemin'),
+    changed      = require('gulp-changed'),
+    pngquant     = require('imagemin-pngquant'),
+    elixirUtil   = require('laravel-elixir/ingredients/commands/Utilities'),
+    Notification = require('laravel-elixir/ingredients/commands/Notification'),
+    elixir       = require('laravel-elixir');
+
+elixir.extend(PLUGIN_NAME, function (src, output, options) {
+  var config = this, srcDir,
+      output = output || './public/img/',
+      // Set default options
+      options = _.extend({
+        // Imagemin options
+        imagemin: {
+          progressive: true,
+          svgoPlugins: [{ removeViewBox: false }],
+          use: [pngquant()]
+        },
+        // Elixir related
+        srcDir: config.assetsDir + 'images'
+      }, options);
+
+  // Use either specified source or search for everything within this directory
+  srcDir = options.srcDir;
+  src = elixirUtil.buildGulpSrc(src, srcDir, '**/*');
+
+  // Create gulp task
+  gulp.task(PLUGIN_NAME, function () {
+    return gulp.src(src)
+      .pipe(changed(output))
+      .pipe(imagemin(options.imagemin))
+      .pipe(gulp.dest(output))
+      .pipe(new Notification().message(PLUGIN_NAME + ' completed running!'))
+      .on('error', function () {
+        return new Notification().error('error occured while running ' + PLUGIN_NAME);
+      });
+  });
+
+  this.registerWatcher(PLUGIN_NAME, srcDir + '/**/*.{png,gif,svg,jpg,jpeg}');
+  return this.queueTask(PLUGIN_NAME);
+});
